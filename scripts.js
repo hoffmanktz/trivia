525740aa5c6ba769b370a3feb2bad9d052ec2340
katie hoffman <kathryn.hoffman@detroitlabs.com>
2017-04-20 15:06:07 -0400

    Adding the updated and fully functional game script

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 	// This handles the API data
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
+	xhr.open("GET", "https://opentdb.com/api.php?amount=10&category=9");
 	
 		xhr.onload = function() {
 			response = JSON.parse(xhr.responseText);
@@ -26,6 +26,7 @@ $(document).ready(function() {
 	var response;
 	var currentQuestion;
 	var questionCount = 10;
+	var questionNumber = 1;
 	var score = 0;
 	var quiz = $('#quiz');
 	var quizOverview = [];
@@ -64,7 +65,7 @@ function printQuestion() {
 		// console.log(currentQuestion);
 
 	// This populates the current question
-	quiz = $('<div id="quiz"><p>' + currentQuestion.question + '</p></div><br>');
+	quiz = $('<div id="quiz"><h3>' + questionNumber + '. ' + currentQuestion.question + '</h3></div><br>');
 	$('#startOver').hide();
 	$('#container').prepend(quiz);
 
@@ -75,7 +76,7 @@ function printQuestion() {
 // This displays the score and the answers from the round - no, this is not pretty at all.
 function displayScore() {
 
-	quiz = $('<div id="quiz"><p>You got ' + score + ' right.</p><p>Question 1:<br>' + quizOverview[0] + '</p><p>The correct answer was:<br>' + quizOverview[1] + '</p><p>Your answer was:<br>' + quizOverview[2] + '<hr><p>Question 2:<br>' + quizOverview[3] + '</p><p>The correct answer was:<br>' + quizOverview[4] + '</p><p>Your answer was:<br>' + quizOverview[5] + '<hr><p>Question 3:<br>' + quizOverview[6] + '</p><p>The correct answer was:<br>' + quizOverview[7] + '</p><p>Your answer was:<br>' + quizOverview[8] + '<hr><p>Question 4:<br>' + quizOverview[9] + '</p><p>The correct answer was:<br>' + quizOverview[10] + '</p><p>Your answer was:<br>' + quizOverview[11] + '<hr><p>Question 5:<br>' + quizOverview[12] + '</p><p>The correct answer was:<br>' + quizOverview[13] + '</p><p>Your answer was:<br>' + quizOverview[14] + '<hr><p>Question 6:<br>' + quizOverview[15] + '</p><p>The correct answer was:<br>' + quizOverview[16] + '</p><p>Your answer was:<br>' + quizOverview[17] + '<hr><p>Question 7:<br>' + quizOverview[18] + '</p><p>The correct answer was:<br>' + quizOverview[19] + '</p><p>Your answer was:<br>' + quizOverview[20] + '<hr><p>Question 8:<br>' + quizOverview[21] + '</p><p>The correct answer was:<br>' + quizOverview[22] + '</p><p>Your answer was:<br>' + quizOverview[23] + '<hr><p>Question 9:<br>' + quizOverview[24] + '</p><p>The correct answer was:<br>' + quizOverview[25] + '</p><p>Your answer was:<br>' + quizOverview[26] + '<hr><p>Question 10:<br>' + quizOverview[27] + '</p><p>The correct answer was:<br>' + quizOverview[28] + '</p><p>Your answer was:<br>' + quizOverview[29] + '</div>');
+	quiz = $('<div id="quiz"><h2>You got <span class="score">' + score + '</span> right.</h2><p><span class="bold">Question 1:</span><br>' + quizOverview[0] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[1] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[2] + '</p><hr><p><span class="bold">Question 2:</span><br>' + quizOverview[3] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[4] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[5] + '<hr><p><span class="bold">Question 3:</span><br>' + quizOverview[6] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[7] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[8] + '<hr><p><span class="bold">Question 4:</span><br>' + quizOverview[9] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[10] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[11] + '<hr><p><span class="bold">Question 5:</span><br>' + quizOverview[12] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[13] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[14] + '<hr><p><span class="bold">Question 6:</span><br>' + quizOverview[15] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[16] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[17] + '<hr><p><span class="bold">Question 7:</span><br>' + quizOverview[18] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[19] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[20] + '<hr><p><span class="bold">Question 8:</span><br>' + quizOverview[21] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[22] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[23] + '<hr><p><span class="bold">Question 9:</span><br>' + quizOverview[24] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[25] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[26] + '<hr><p><span class="bold">Question 10:</span><br>' + quizOverview[27] + '</p><p><span class="bold">The correct answer was:</span><br>' + quizOverview[28] + '</p><p><span class="bold">Your answer was:</span><br>' + quizOverview[29] + '</div>');
 	$('#container').prepend(quiz);
 
 }
@@ -103,6 +104,7 @@ $('body').on('click', '#next', function() {
 		if(questionCount > 0) {
 			quiz.remove();
 			questionCount--;
+			questionNumber++;
 			$('#startOver').hide();
 		}
 
